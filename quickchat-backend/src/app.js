1f6ad3e8a58f7ef825fa04eed6e621f16c2d3555
Sameersid1 <sameersiddique953@gmail.com>
2026-04-18 02:20:33 +0530

    debugged

--- quickchat-backend/src/app.js
+++ quickchat-backend/src/app.js
@@ -12,7 +12,7 @@ app.use(cors({
     methods:["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
     allowedHeaders:["Content-Type","Authorization"]          //in postman
 }))
-
+app.options("*", cors());
 //basic configuration
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
