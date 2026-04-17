273f8f0fac62a0898bba2b985730707ad3a71496
Sameersid1 <sameersiddique953@gmail.com>
2026-04-18 02:08:30 +0530

    cors debug

--- quickchat-backend/src/app.js
+++ quickchat-backend/src/app.js
@@ -4,12 +4,6 @@ import cookieParser from "cookie-parser"
 
 const app=express()
 
-//basic configuration
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static("public"))
-
-
 //cors configuration
 //Browser ko clearly bata rahe ho ki kaun-kaun se frontend, kaise, kis type ke request bhej sakte hain.
 app.use(cors({
@@ -18,6 +12,13 @@ app.use(cors({
     methods:["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
     allowedHeaders:["Content-Type","Authorization"]          //in postman
 }))
+
+//basic configuration
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+
+
 
 //common middleware
 app.use(cookieParser())
