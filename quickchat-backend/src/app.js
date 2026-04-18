716f671c6797ab4ace0365153044e500f9ad527b
Sameersid1 <sameersiddique953@gmail.com>
2026-04-18 15:04:35 +0530

    checking deploy

--- quickchat-backend/src/app.js
+++ quickchat-backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from "cookie-parser"
 
+console.log("🔥 NEW VERSION DEPLOYED");
 const app=express()
 app.get("/", (req, res) => {
   res.send("ROOT WORKING");
