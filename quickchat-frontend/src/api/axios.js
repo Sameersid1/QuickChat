bd2bbe2ad6fedab3643d532d695463447b3b302d
Sameersid1 <sameersiddique953@gmail.com>
2026-05-01 15:56:09 +0530

    cors bug2

--- quickchat-frontend/src/api/axios.js
+++ quickchat-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://quickchat-backend.onrender.com/api/v1", // backend URL
+  baseURL: "https://quickchat-backend-0gnj.onrender.com/api/v1", // backend URL
   withCredentials: true, // if using cookies
 });
 
